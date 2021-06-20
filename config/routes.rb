1437b21a5283c82f31b80621cf62fb6fb15911f5
joota34256 <fuji_8761@yahoo.co.jp>
2021-06-20 21:12:24 +0900

    6/20　修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-<<<<<<< Updated upstream
   resources :events
-=======
-  # redources :events do
-    
-  # end
->>>>>>> Stashed changes
+  
 end
