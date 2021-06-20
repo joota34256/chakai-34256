3cf6ee35160076497cbfdb6b0b25899721acc00f
joota34256 <fuji_8761@yahoo.co.jp>
2021-06-20 21:11:21 +0900

    cc

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   root to:"events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+<<<<<<< Updated upstream
   resources :events
+=======
+  # redources :events do
+    
+  # end
+>>>>>>> Stashed changes
 end
